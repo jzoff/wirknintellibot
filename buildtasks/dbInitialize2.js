163a1d61a0b4cb3c40e2144a1d7adcc560c8bef9
Jas Kang <jaspreet.kang@threepointturn.com>
2015-11-16 21:34:29 -0500

    test2

---
+++ buildtasks/dbInitialize2.js
@@ -0,0 +1,767 @@
+var Activity = require('../model/Activity.js');
+var NextActivity = require('../model/NextActivity.js');
+var config = require('../config/db');
+var mongoose = require('mongoose');
+//mongoose.connect(config.connectionString);
+mongoose.connect('mongodb://localhost/mybot');
+
+var activity = new Activity({
+    id:  0,
+    plugin: 'rand.js',
+    data: {
+        type: 'text',
+        desc: 'Personality'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  1,
+    plugin: 'greeting.js',
+    data: {
+        type: 'text',
+        desc: 'Hi. My name is Cynthia'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  2,
+    plugin: 'greeting.js',
+    data: {
+        type: 'text',
+        desc: 'Hi. My name is Jon'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  3,
+    plugin: 'greeting.js',
+    data: {
+        type: 'text',
+        desc: 'Hi. My name is Pedro'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  4,
+    plugin: 'kikask.js',
+    data: {
+        type: 'text',
+        desc: 'First question, When you meet up with friends, you like to\n(a) Go Shopping\n(b) Eat, drink, be merry\n(c) Go to a concert\n(d) Do whatever they want'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  5,
+    plugin: 'kikask.js',
+    data: {
+        type: 'text',
+        desc: 'Describe yourself in one word\n(a) Fashionable\n(b) Charming\n(c) Adventurous\n(d) Adaptable'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  6,
+    plugin: 'kikask.js',
+    data: {
+        type: 'text',
+        desc: 'I\'ve always wanted to be friends with:\n(a) Floyd Mayweather\n(b) Gordon Ramsey\n(c) Katy Perry\n(d) Kim Kardashian'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  7,
+    plugin: 'kikask.js',
+    data: {
+        type: 'text',
+        desc: 'You\'re going to see a movie, which one do you pick:\n(a) The Devil Wears Prada\n(b) The Wolf of Wall Street\n(c) Depicable Me\n(d) I can\'t decide'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  8,
+    plugin: 'kikask.js',
+    data: {
+        type: 'text',
+        desc: 'If you found $100, where would you spend it?\n(a) H&M\n(b) Chipotle Mexican Grill\n(c) Disneyland\n(d) Somewhere at the mall'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  9,
+    plugin: 'kikask.js',
+    data: {
+        type: 'text',
+        desc: 'Your biggest frustration is\n(a) Credit card bills\n(b) Slow service\n(c) Waiting in line\n(d) Hashtags'
+    }
+});
+activity.save();
+
+activity = new Activity({
+    id:  10,
+    plugin: 'final.js',
+    data: {
+        type: 'text',
+        desc: 'Okay, great.  Give us a minute while we calculate your results'
+    }
+});
+console.log(activity);
+activity.save();
+
+//Q4 a,b,c,d
+activity = new Activity({
+    id:  4100,//a
+    plugin: 'score.js',
+    data: {
+       type: 'text',         desc:'1'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  4200,//b
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'2'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  4300,//c
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'3'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  4400,//d
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'4'
+    }
+});
+activity.save();
+
+//Q5 a,b,c,d
+activity = new Activity({
+    id:  5100,//a
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'1'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  5200,//b
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'2'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  5300,//c
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'3'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  5400,//d
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'4'
+    }
+});
+activity.save();
+
+//6 a,b,c,d
+activity = new Activity({
+    id:  6100,//a
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'1'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  6200,//b
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'2'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  6300,//c
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'3'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  6400,//d
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'4'
+    }
+});
+activity.save();
+
+//7 a,b,c,d
+activity = new Activity({
+    id:  7100,//a
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'1'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  7200,//b
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'2'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  7300,//c
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'3'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  7400,//d
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'4'
+    }
+});
+activity.save();
+
+//8 a,b,c,d
+activity = new Activity({
+    id:  8100,//a
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'1'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  8200,//b
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'2'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  8300,//c
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'3'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  8400,//d
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'4'
+    }
+});
+activity.save();
+
+//9 a,b,c,d
+activity = new Activity({
+    id:  9100,//a
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'1'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  9200,//b
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'2'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  9300,//c
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'3'
+    }
+});
+activity.save();
+activity = new Activity({
+    id:  9400,//d
+    plugin: 'score.js',
+    data: {
+        type: 'text',         desc:'4'
+    }
+});
+activity.save();
+console.log(activity);
+
+var nextActivity = new NextActivity({
+    thisActivityId:  0,
+    nextActivityId: 1,
+    condition: "username=='cynthia'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  0,
+    nextActivityId: 2,
+    condition: "username=='jon'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  0,
+    nextActivityId: 3,
+    condition: "username=='pedro'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  1,
+    nextActivityId: 4,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  2,
+    nextActivityId: 4,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  3,
+    nextActivityId: 4,
+    condition: 'true'
+});
+nextActivity.save();
+
+/*nextActivity = new NextActivity({
+    thisActivityId:  4,
+    nextActivityId: 5,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  5,
+    nextActivityId: 6,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  6,
+    nextActivityId: 7,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  7,
+    nextActivityId: 8,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  8,
+    nextActivityId: 9,
+    condition: 'true'
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  9,
+    nextActivityId: 10,
+    condition: 'true'
+});
+nextActivity.save();
+*/
+//Q4 abcd
+nextActivity = new NextActivity({
+    thisActivityId:  4,
+    nextActivityId: 4100,
+    condition: "answer=='a'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  4,
+    nextActivityId: 4200,
+    condition: "answer=='b'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  4,
+    nextActivityId: 4300,
+    condition: "answer=='c'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  4,
+    nextActivityId: 4400,
+    condition: "answer=='d'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  4100,
+    nextActivityId: 5,
+    condition: true
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  4200,
+    nextActivityId: 5,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  4300,
+    nextActivityId: 5,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId: 4400,
+    nextActivityId: 5,
+    condition: true
+});
+nextActivity.save();
+
+//Q5 abcd
+nextActivity = new NextActivity({
+    thisActivityId:  5,
+    nextActivityId: 5100,
+    condition: "answer=='a'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  5,
+    nextActivityId: 5200,
+    condition: "answer=='b'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  5,
+    nextActivityId: 5300,
+    condition: "answer=='c'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  5,
+    nextActivityId: 5400,
+    condition: "answer=='d'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  5100,
+    nextActivityId: 6,
+    condition: true
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  5200,
+    nextActivityId: 6,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  5300,
+    nextActivityId: 6,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  5400,
+    nextActivityId: 6,
+    condition: true
+});
+nextActivity.save();
+
+//Q6 abcd
+nextActivity = new NextActivity({
+    thisActivityId:  6,
+    nextActivityId: 6100,
+    condition: "answer=='a'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  6,
+    nextActivityId: 6200,
+    condition: "answer=='b'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  6,
+    nextActivityId: 6300,
+    condition: "answer=='c'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  6,
+    nextActivityId: 6400,
+    condition: "answer=='d'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  6100,
+    nextActivityId: 7,
+    condition: true
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  6200,
+    nextActivityId: 7,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  6300,
+    nextActivityId: 7,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  6400,
+    nextActivityId: 7,
+    condition: true
+});
+nextActivity.save();
+
+//Q7 abcd
+nextActivity = new NextActivity({
+    thisActivityId:  7,
+    nextActivityId: 7100,
+    condition: "answer=='a'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  7,
+    nextActivityId: 7200,
+    condition: "answer=='b'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  7,
+    nextActivityId: 7300,
+    condition: "answer=='c'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  7,
+    nextActivityId: 7400,
+    condition: "answer=='d'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  7100,
+    nextActivityId: 8,
+    condition: true
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  7200,
+    nextActivityId: 8,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  7300,
+    nextActivityId: 8,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  7400,
+    nextActivityId: 8,
+    condition: true
+});
+nextActivity.save();
+
+//Q8 abcd
+nextActivity = new NextActivity({
+    thisActivityId:  8,
+    nextActivityId: 8100,
+    condition: "answer=='a'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  8,
+    nextActivityId: 8200,
+    condition: "answer=='b'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  8,
+    nextActivityId: 8300,
+    condition: "answer=='c'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  8,
+    nextActivityId: 8400,
+    condition: "answer=='d'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  8100,
+    nextActivityId: 9,
+    condition: true
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  8200,
+    nextActivityId: 9,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  8300,
+    nextActivityId: 9,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  8400,
+    nextActivityId: 9,
+    condition: true
+});
+nextActivity.save();
+
+//Q9 abcd
+nextActivity = new NextActivity({
+    thisActivityId:  9,
+    nextActivityId: 9100,
+    condition: "answer=='a'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  9,
+    nextActivityId: 9200,
+    condition: "answer=='b'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  9,
+    nextActivityId: 9300,
+    condition: "answer=='c'"
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  9,
+    nextActivityId: 9400,
+    condition: "answer=='d'"
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  9100,
+    nextActivityId: 10,
+    condition: true
+});
+nextActivity.save();
+
+nextActivity = new NextActivity({
+    thisActivityId:  9200,
+    nextActivityId: 10,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  9300,
+    nextActivityId: 10,
+    condition: true
+});
+nextActivity.save();
+nextActivity = new NextActivity({
+    thisActivityId:  9400,
+    nextActivityId: 10,
+    condition: true
+});
+nextActivity.save();
+console.log(nextActivity);
+
+mongoose.disconnect();
+
+/**
+ * workflow
+ * id   name
+ * 1    Personality
+ * 2    Profile
+ */
+/*
+ workflow instance
+ id  workflowid workparentId
+
+ user
+ date
+ 1
+ null
+ workflowinstance added
+ return workflowinstance id -instance started for workflow 1
+
+ get workflowInstance which 1
+ if (no active activity)
+ {
+ create activity instance and mark it active
+ execute the start activity
+ }
+ else
+ execute current active activity
+ * */
+
+
+
+/*from    to  value
+ 0       1   cynthia
+ 0       2   jon
+ 0       3   pedro
+ 2       4   true
+ 4       5   true
+ 5       6
+ 6       7
+ 7       8*/
+
---
+++ helpers/testengine2.js
@@ -0,0 +1,320 @@
+var Activity = require('../model/Activity.js');
+var NextActivity = require('../model/NextActivity.js');
+var User = require('../model/User.js');
+
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/mybot');
+
+var getActivityFromDb = function(id, cb) {
+    var query = Activity.findOne({id: id});
+    query.exec(function (err, startActivity) {
+        if (err) {
+            console.log(err);//return res.send(400);
+        }
+
+        var query = NextActivity.find({thisActivityId : id});
+        query.exec(function (err, nextActivities) {
+            if (err) {
+                console.log(err);//return res.send(400);
+            }
+
+            /*var currentActivity = {id: id, plugin: startActivity.plugin};
+             currentActivity.nextActivities = nextActivities || [];
+             cb(currentActivity);*/
+
+            var returnVals = {
+                currentActivity: startActivity,
+                nextActivities: nextActivities || []
+            };
+            //console.log(startActivity);
+            //console.log(nextActivities);
+            cb(returnVals);
+            //mongoose.disconnect();
+            //return res.send(200, currentActivity);
+        });
+    });
+}
+
+var getUserFromDb = function(username, cb) {
+    var query = User.findOne({username: username});
+    query.exec(function (err, user) {
+        if (err) {
+            console.log(err);//return res.send(400);
+        }
+        if(user === null) {
+            console.log('no user');
+            user = new User({
+                username: username,
+                current: null,
+                value: 0
+            });
+            user.save();
+        }
+        var returnVals = {
+            user: user
+        };
+        cb(returnVals);
+        //return res.send(200, currentActivity);
+    });
+}
+var updateUserDb = function(username, activity, cb) {
+    //console.log('in update',username, activity);
+    //A.findOneAndUpdate(conditions, update, options, callback) // executes
+    var query = User.findOneAndUpdate({ username: username }, { current: activity }, {new: true});//, function (err, user) {
+    query.exec(function (err, user) {
+        if (err) {
+            console.log(err);//return res.send(400);
+        }
+        if(user === null) {
+            console.log('no user');
+        }
+        var returnVals = {
+            user: user
+        };
+        cb(returnVals);
+        //mongoose.disconnect();
+        //return res.send(200, currentActivity);
+    });
+}
+
+
+var Engine = {}
+
+Engine.execute = function(username,datas,input,cb) {
+    //console.log(datas);
+    var arr = []
+    if (datas !== null){
+        console.log('currentId:         ' + datas.currentActivity.id);
+        console.log('plugin             ' + datas.currentActivity.plugin);
+        //console.log('desc             ' + datas.currentActivity.data.desc);
+
+        var plugin = require('../plugins/' + datas.currentActivity.plugin);
+        plugin.returnFunc(username, datas.currentActivity.data.desc,datas.nextActivities.length>0 ? datas.nextActivities[0].nextActivityId:-1);
+        //console.log('Question:          ' + plugin.returnFunc(username, datas.currentActivity.data.desc));
+
+        console.log('Count:             ' + datas.nextActivities.length);
+
+        //plugin.returnFunc(username, datas.currentActivity.data.desc)();
+        console.log(datas.nextActivities)
+        console.log(input)
+        for (var i = 0; i < datas.nextActivities.length; i++) {
+            if (eval(datas.nextActivities[i].condition)) {
+                arr.push(datas.nextActivities[i].nextActivityId)
+                console.log(datas.nextActivities[i].nextActivityId)
+            }
+        }
+    }
+    // return arr
+    /*arr.forEach(function(v) {
+     console.log('nextId:            ' + v);
+     });*/
+    cb(arr);
+}
+
+//third test
+Engine.doYourThing = function(username, input) {
+    /*var username = 'cynthia';
+    var answer = 'a';*/
+    getUserFromDb(username,function(returnUser){//get current activity for the user
+        console.log('username:' +returnUser.user.username, 'currentId:' + returnUser.user.current );
+        var username = returnUser.user.username;
+        var activity = returnUser.user.current === null ? 0 : returnUser.user.current ;
+
+        getActivityFromDb(activity, function(returnVals) {//get all nextactivity based on current
+
+            Engine.execute(username,returnVals,input,function(arr){//get matching nextactivity based on condition
+                if (arr.length <= 0) {
+                    console.log('done for:'+ username);
+                    mongoose.disconnect();
+                }
+                else {
+                    arr.forEach(function (v) {
+                        console.log('nextId:            ' + v);
+                    });
+
+                    updateUserDb(username, arr[0], function (user) { //update user.current activity
+                        console.log(username, arr[0], user.user.current, user.user.value, user.user.username);
+                        mongoose.disconnect();
+                    });
+                }
+
+            });
+
+        })
+    })
+}
+//Engine.doYourThing();
+
+module.exports = Engine;
+//first test
+//Engine.execute(arrOut);
+
+//Second test
+/*var doYourThing = function() {
+ getFromDb(0, function(returnVals) {
+ Engine.execute(returnVals);
+ })
+ }
+ doYourThing();*/
+
+
+/*
+ var getFromDb = function(id, cb) {
+ // get from db
+ Activity.findOne({id: id}).exec(function(err,docs) {
+ if(err) {
+ console.log(err);
+ }
+ else {
+ //console.log('*********\n' + docs + '\n*****');
+ var returnVals = {
+ startActivity: docs,
+ NextActivity1: {
+ c: 1,
+ n: 2,
+ condition: "1 + 1 == 2"}
+ };
+ cb(returnVals);
+ mongoose.disconnect();
+ }
+ });
+ }
+ var Activity1 = {
+ id: 1,
+ plugin: "pq",
+ data: {
+ type: "text"
+ }
+ }
+
+ var Activity2 = {
+ id: 2,
+ plugin: "pq",
+ data: {
+ type: "text"
+ }
+ }
+
+ var NextActivity1 = {
+ c: 1,
+ n: 2,
+ condition: "1 + 1 == 2"
+ }
+
+ var NextActivity2 = {
+ c: 1,
+ n: 3,
+ condition: "1 + 1 == 2"
+ }
+
+ var a1 = Activity1;
+ var a2 = Activity2;
+ var n1 = NextActivity1;
+ var n2 = NextActivity2;
+
+ var arrOut = [
+ n1, n2
+ ]
+
+ /////////////
+ async.parallel([
+ function(cb){
+ users.find({}, cb);
+ },
+ function(cb){
+ articles.find({}, cb);
+ }
+ ], function(results){
+ // results contains both users and articles
+ });
+
+
+ /////////////////////
+ var query = db.SheetsModel.findOne({_id: sheet_id});
+ query.exec(function(err, sheet) {
+ if (err) {
+ return res.send(400);
+ }
+
+ var query = db.FriendsModel.find({sheet_id: sheet_id});
+ query.exec(function(err, friends) {
+ if (err) {
+ return res.send(400);
+ }
+
+ var query = db.ExpensesModel.find({sheet_id: sheet_id});
+ query.sort('-date');
+ query.exec(function(err, expenses) {
+ if (err) {
+ return res.send(400);
+ }
+
+ var sheetData = {_id: sheet._id, name: sheet.name};
+ sheetData.friends = friends || [];
+ sheetData.expenses = expenses || [];
+
+ return res.send(200, sheetData);
+ });
+ });
+ });
+ /////////////////////
+
+ Async.parallel([
+
+ //Read sheets data from Sheets
+ function(callback) {
+ var query = db.SheetsModel.findOne({_id: sheet_id});
+ query.exec(function(err, sheet) {
+ if (err) {
+ callback(err);
+ }
+
+ callback(null, sheet);
+ });
+ },
+
+ //Read friends data from Friends
+ function(callback) {
+ var query = db.FriendsModel.find({sheet_id: sheet_id});
+ query.exec(function(err, friends) {
+ if (err) {
+ callback(err);
+ }
+
+ callback(null, friends);
+ });
+ },
+
+ //Read expenses data from Expenses
+ function(callback) {
+ var query = db.ExpensesModel.find({sheet_id: sheet_id});
+ query.sort('-date');
+ query.exec(function(err, expenses) {
+ if (err) {
+ callback(err);
+ }
+
+ callback(null, expenses);
+ });
+ }
+ ],
+
+ //Compute all results
+ function(err, results) {
+ if (err) {
+ console.log(err);
+ return res.send(400);
+ }
+
+ if (results == null || results[0] == null) {
+ return res.send(400);
+ }
+
+ //results contains [sheets, Friends, Expenses]
+ var sheetData = {_id: results[0]._id, name: results[0].name};
+ sheetData.friends = results[1] || [];
+ sheetData.expenses = results[2] || [];
+
+ return res.send(200, sheetData);
+ });
+ */
